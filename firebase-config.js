ce1fa7cf1338b9a763890b734ab623ff10f0366f
SkFeatures <shaikh.sabbir.ali0@gmail.com>
2026-08-13 15:08:38 +0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDdyowsSxvCTzrSsCRicW8JCL44rz1zAoQ",
+    authDomain: "expendies-8d466.firebaseapp.com",
+    projectId: "expendies-8d466",
+    storageBucket: "expendies-8d466.firebasestorage.app",
+    messagingSenderId: "710845937485",
+    appId: "1:710845937485:web:4145df9ca6ddfad5506904",
+    measurementId: "G-WT62N5LLPP"
+};
